97a839e6827a2d9be5efdb043fc4ea6b27b9b95d
Andrew Stanton <up683693@myport.ac.uk>
2019-11-09 00:13:21 +0000

    Fixing the add header row to the string[][]

--- utils/orderedLists.js
+++ utils/orderedLists.js
@@ -1,4 +1,4 @@
-/** Use the headers to put the line into the desired order
+/** Use the headers to put the line into the desired order and adds header row
  *
  * @param {Object[]} data A list of line representations
  * @param {string[]} headers The headers in the chosen order
@@ -7,5 +7,6 @@ export default function(data, headers) {
 	const sortedData = data.map((row) =>
 		headers.map((cellName) => row[cellName])
 	);
-	return sortedData.unshift(headers);
+	sortedData.unshift(headers);
+	return sortedData;
 }
