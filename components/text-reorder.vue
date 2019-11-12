78d76a6da8d0c53e05a6ebe433a9e6457f33ec4d
Andrew Stanton <up683693@myport.ac.uk>
2019-11-12 22:15:52 +0000

    Validate column text input
    Checks items. Lists invalid and unsed columns.
    Button to add column to the input list

--- components/text-reorder.vue
+++ components/text-reorder.vue
@@ -8,7 +8,7 @@
 		</p>
 		<form
 			class="form"
-			@submit.prevent="loadSortData"
+			@submit.prevent="validateUserInput"
 		>
 			<textarea
 				v-model="userInput"
@@ -17,14 +17,39 @@
 				cols="30"
 				rows="10"
 			></textarea>
+
 			<button
 				class="button is-primary"
 				type="submit"
 			>
-				Load order
+				Validate input
 			</button>
 		</form>
-		<section>{{ rejectedInput }}</section>
+		<div
+			v-if="rejectedInput.length || unusedInput.length"
+			class="columns"
+		>
+			<section class="column">
+				<h4>Rejected Input</h4>
+				<p>These are options that don't match the columns in the given file</p>
+				{{ rejectedInput }}
+			</section>
+			<section class="column">
+				<h4>Unused Input</h4>
+				<p>These are options from the file that aren't listed above</p>
+				<ul>
+					<li
+						v-for="(unused) in unusedInputSorted"
+						:key="unused"
+						@click="clickedOnUnused(unused)"
+					>
+						<button class="button">
+							{{ unused }}
+						</button>
+					</li>
+				</ul>
+			</section>
+		</div>
 	</section>
 </template>
 
@@ -33,37 +58,57 @@ export default {
 	props: {
 		dataReceived: {
 			required: true,
-			type: Array
+			type: Array,
 		},
 		value: {
 			type: Array,
-			required: true
-		}
+			required: true,
+		},
 	},
 	data() {
 		return {
 			userInput: 'war\nfamine\nplague\ndeath',
 			rejectedInput: [],
+			unusedInput: [],
 		};
 	},
 	computed: {
-		validationPassed(){
+		validationPassed() {
 			return !this.rejectedInput.length;
-		}
+		},
+		unusedInputSorted() {
+			// sort uses side effect
+			return Array.from(this.unusedInput).sort();
+		},
 	},
 	methods: {
-		loadSortData() {
+		validateUserInput() {
 			console.log(this.userInput);
 
 			let currentInput = this.userInput.split('\n');
-			const validOptions = currentInput.filter((inputItem) => this.dataReceived.includes(inputItem));
-			this.rejectedInput =  currentInput.filter((inputItem) => !this.dataReceived.includes(inputItem));
-			this.userInput = validOptions.join('\n');
-		}
-	}
+
+			this.userInput = currentInput
+				.filter((inputItem) => this.dataReceived.includes(inputItem))
+				.join('\n');
+
+			this.rejectedInput = currentInput.filter(
+				(inputItem) => !this.dataReceived.includes(inputItem)
+			);
+
+			this.unusedInput = this.dataReceived.filter(
+				(inputItem) => !currentInput.includes(inputItem)
+			);
+		},
+		clickedOnUnused(item) {
+			let [output] = this.unusedInput.splice(
+				this.unusedInput.indexOf(item),
+				1
+			);
+			this.userInput = this.userInput.concat('\n', output).trim();
+		},
+	},
 };
 </script>
 
 <style scoped>
-
 </style>
